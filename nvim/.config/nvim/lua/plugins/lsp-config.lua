72a78d0270ed15940566083b20f6c8c475d19fc0
coderzawad <zawad@devsnest.llc>
2025-01-24 00:46:13 +0600

    Add ts_ls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,20 +20,45 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
+
+      -- LSP servers setup
       lspconfig.tailwindcss.setup({
         capabilities = capabilities
       })
-      lspconfig.ruby_lsp.setup({
-        capabilities = capabilities,
-        cmd = { "/home/typecraft/.asdf/shims/ruby-lsp" }
-      })
+
       lspconfig.html.setup({
         capabilities = capabilities
       })
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
 
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- Disable tsserver formatting, use prettier instead
+          client.server_capabilities.documentFormattingProvider = false
+        end
+      })
+
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- Enable Eslint's formatting
+          client.server_capabilities.documentFormattingProvider = true
+        end
+      })
+
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+
+      -- Key mappings for LSP
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
